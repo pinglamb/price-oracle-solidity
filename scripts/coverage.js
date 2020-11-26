009e4fc64ae0819129ef21a0ca87f7341303100d
pinglamb <pinglambs@gmail.com>
2020-11-27 00:50:01 +0800

    🔥 Removed Migration.sol

--- scripts/coverage.js
+++ scripts/coverage.js
@@ -4,7 +4,7 @@ const { execSync } = require('child_process');
 const { runCoverage } = require('@openzeppelin/test-environment');
 
 async function main() {
-  await runCoverage(['mocks/', 'Migrations.sol'], 'yarn compile:coverage', ['./node_modules/.bin/mocha']);
+  await runCoverage(['mocks/'], 'yarn compile:coverage', ['./node_modules/.bin/mocha']);
 
   if (process.env.CI) {
     execSync('curl -s https://codecov.io/bash | bash -s -- -C "$GITHUB_SHA"', { stdio: 'inherit' });
