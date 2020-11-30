7b3c084c82a9961ef915fcc4fa591193c9df42a9
pinglamb <pinglambs@gmail.com>
2020-11-30 19:56:35 +0800

    🔒 Fixed all slither warning

--- contracts/ChainlinkProxyPriceProvider.sol
+++ contracts/ChainlinkProxyPriceProvider.sol
@@ -5,6 +5,7 @@
 // - Remove fallbackOracle
 // - Store ethAddress (for potential WETH support)
 // - Add getETHPriceInAsset
+// - Followed solidity naming conventions
 
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.11;
@@ -26,48 +27,48 @@ contract ChainlinkProxyPriceProvider is IPriceOracleGetter, Ownable {
 
     event AssetSourceUpdated(address indexed asset, address indexed source);
 
-    mapping(address => IChainlinkAggregator) private assetsSources;
-    address private ethAddress;
+    mapping(address => IChainlinkAggregator) private _assetsSources;
+    address private _ethAddress;
 
     /// @notice Constructor
-    /// @param _assets The addresses of the assets
-    /// @param _sources The address of the source of each asset
-    constructor(address[] memory _assets, address[] memory _sources) public {
-        internalSetAssetsSources(_assets, _sources);
+    /// @param assets The addresses of the assets
+    /// @param sources The address of the source of each asset
+    constructor(address[] memory assets, address[] memory sources) public {
+        internalSetAssetsSources(assets, sources);
     }
 
     /// @notice External function called by the owner to set or replace sources of assets
-    /// @param _assets The addresses of the assets
-    /// @param _sources The address of the source of each asset
-    function setAssetSources(address[] calldata _assets, address[] calldata _sources) external onlyOwner {
-        internalSetAssetsSources(_assets, _sources);
+    /// @param assets The addresses of the assets
+    /// @param sources The address of the source of each asset
+    function setAssetSources(address[] calldata assets, address[] calldata sources) external onlyOwner {
+        internalSetAssetsSources(assets, sources);
     }
 
     /// @notice Internal function to set the sources for each asset
-    /// @param _assets The addresses of the assets
-    /// @param _sources The address of the source of each asset
-    function internalSetAssetsSources(address[] memory _assets, address[] memory _sources) internal {
-        require(_assets.length == _sources.length, 'INCONSISTENT_PARAMS_LENGTH');
-        for (uint256 i = 0; i < _assets.length; i++) {
-            assetsSources[_assets[i]] = IChainlinkAggregator(_sources[i]);
-            emit AssetSourceUpdated(_assets[i], _sources[i]);
+    /// @param assets The addresses of the assets
+    /// @param sources The address of the source of each asset
+    function internalSetAssetsSources(address[] memory assets, address[] memory sources) internal {
+        require(assets.length == sources.length, 'INCONSISTENT_PARAMS_LENGTH');
+        for (uint256 i = 0; i < assets.length; i++) {
+            _assetsSources[assets[i]] = IChainlinkAggregator(sources[i]);
+            emit AssetSourceUpdated(assets[i], sources[i]);
         }
     }
 
     /// @notice Gets an asset price by address
-    /// @param _asset The asset address
-    function getAssetPrice(address _asset) public view override returns (uint256) {
-        IChainlinkAggregator source = assetsSources[_asset];
-        if (_asset == ethAddress) {
+    /// @param asset The asset address
+    function getAssetPrice(address asset) public view override returns (uint256) {
+        IChainlinkAggregator source = _assetsSources[asset];
+        if (asset == _ethAddress) {
             return 1 ether;
         } else {
             // Require the asset has registered source
             require(address(source) != address(0), 'SOURCE_IS_MISSING');
 
-            int256 _price = IChainlinkAggregator(source).latestAnswer();
-            require(_price > 0, 'INVALID_PRICE');
+            int256 price = IChainlinkAggregator(source).latestAnswer();
+            require(price > 0, 'INVALID_PRICE');
 
-            return uint256(_price);
+            return uint256(price);
         }
     }
 
@@ -75,47 +76,47 @@ contract ChainlinkProxyPriceProvider is IPriceOracleGetter, Ownable {
     ///         the number is not reounded by the asset actual number of decimal places
     ///         expect proper rounding is done outside of this function and getAssetPrice
     ///         returns number in WAD format
-    /// @param _asset The asset address
-    function getETHPriceInAsset(address _asset) external view override returns (uint256) {
-        if (_asset == ethAddress) {
+    /// @param asset The asset address
+    function getETHPriceInAsset(address asset) external view override returns (uint256) {
+        if (asset == _ethAddress) {
             return 1 ether;
         } else {
-            uint256 _assetPrice = getAssetPrice(_asset);
-            uint256 _price = _assetPrice.reciprocal();
-            require(_price > 0, 'INVALID_PRICE');
+            uint256 assetPrice = getAssetPrice(asset);
+            uint256 price = assetPrice.reciprocal();
+            require(price > 0, 'INVALID_PRICE');
 
-            return _price;
+            return price;
         }
     }
 
     /// @notice Gets a list of prices from a list of assets addresses
-    /// @param _assets The list of assets addresses
-    function getAssetsPrices(address[] calldata _assets) external view returns (uint256[] memory) {
-        uint256[] memory prices = new uint256[](_assets.length);
-        for (uint256 i = 0; i < _assets.length; i++) {
-            prices[i] = getAssetPrice(_assets[i]);
+    /// @param assets The list of assets addresses
+    function getAssetsPrices(address[] calldata assets) external view returns (uint256[] memory) {
+        uint256[] memory prices = new uint256[](assets.length);
+        for (uint256 i = 0; i < assets.length; i++) {
+            prices[i] = getAssetPrice(assets[i]);
         }
         return prices;
     }
 
     /// @notice Gets the address of the source for an asset address
-    /// @param _asset The address of the asset
+    /// @param asset The address of the asset
     /// @return address The address of the source
-    function getSourceOfAsset(address _asset) external view returns (address) {
-        return address(assetsSources[_asset]);
+    function getSourceOfAsset(address asset) external view returns (address) {
+        return address(_assetsSources[asset]);
     }
 
     /// @notice Sets the ethAddress, in aave the ethAddress is a special representation for ETH,
     ///         generalized to be configurable per system, can be for example WETH address
-    /// @param _ethAddress The address of ETH
-    function setETHAddress(address _ethAddress) external onlyOwner {
-        require(_ethAddress != address(0), 'ADDRESS_IS_ZERO');
-        ethAddress = _ethAddress;
+    /// @param ethAddress The address of ETH
+    function setETHAddress(address ethAddress) external onlyOwner {
+        require(ethAddress != address(0), 'ADDRESS_IS_ZERO');
+        _ethAddress = ethAddress;
     }
 
     /// @notice Gets the ethAddress
     /// @return address The ethAddress
     function getETHAddress() external view returns (address) {
-        return ethAddress;
+        return _ethAddress;
     }
 }
